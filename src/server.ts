5f0132819d91f7b3a2299155ebcd2e7341bbe061
Luiz Rodrigues Junior <junior.luiz182@gmail.com>
2022-05-06 17:35:11 -0300

    iniciando o backend

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3333, () => {
+  console.log('HTTP server running!')
+})
+
+app.get('/users', (req, res) => {
+  return res.send('Hello World')
+})
